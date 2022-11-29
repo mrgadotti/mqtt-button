10e5e52940271d284e55f26ae909221de68f63c7
Marcelo Gadotti <eng.marcelogadotti@gmail.com>
2022-11-29 09:38:04 -0300

    chore: update to show payload on list
    Signed-off-by: Marcelo Gadotti <eng.marcelogadotti@gmail.com>

--- app/src/main/java/br/com/hussan/coffeeiot/ui/coffee/CoffeeActivity.kt
+++ app/src/main/java/br/com/hussan/coffeeiot/ui/coffee/CoffeeActivity.kt
@@ -5,24 +5,30 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import br.com.hussan.coffeeiot.R
 import br.com.hussan.coffeeiot.extensions.color
 import br.com.hussan.coffeeiot.extensions.hide
 import br.com.hussan.coffeeiot.extensions.show
 import br.com.hussan.coffeeiot.mqtt.MqttClient
-import kotlinx.android.synthetic.main.activity_coffee.btnCoffee
-import kotlinx.android.synthetic.main.activity_coffee.imgCoffee
+//import kotlinx.android.synthetic.main.activity_coffee.btnCoffee
+//import kotlinx.android.synthetic.main.activity_coffee.imgCoffee
 import kotlinx.android.synthetic.main.activity_coffee.lytRoot
-import kotlinx.android.synthetic.main.activity_coffee.progressBar
+
 import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.json.JSONObject
 
 class CoffeeActivity : AppCompatActivity() {
 
     private lateinit var broker: String
     private lateinit var topic: String
+    private var users = arrayOf<String?>()
+
     private val prefs: PrefsRespository by lazy {
         PrefsRespository(this)
     }
@@ -44,9 +50,9 @@ class CoffeeActivity : AppCompatActivity() {
         getLocalData()
 
         connectAndSubscribe()
-
+/*
         btnCoffee.setOnClickListener {
-            progressBar.show()
+
             onOff = if (!onOff) {
                 mqttClient.publishMessage(topic, "on")
                 true
@@ -54,7 +60,7 @@ class CoffeeActivity : AppCompatActivity() {
                 mqttClient.publishMessage(topic, "off")
                 false
             }
-        }
+        }*/
     }
 
     private fun getLocalData() {
@@ -67,8 +73,31 @@ class CoffeeActivity : AppCompatActivity() {
         mqttClient.setCallBack(arrayOf(topic), ::updateButton)
     }
 
+    fun <T> append(arr: Array<T>, element: T): Array<T?> {
+        val array = arr.copyOf(arr.size + 1)
+        array[arr.size] = element
+        return array
+    }
     private fun updateButton(topic: String, message: MqttMessage) {
-        imgCoffee.show()
+        //imgCoffee.show()
+
+        val arrayAdapter: ArrayAdapter<*>
+
+        val mListView = findViewById<ListView>(R.id.userlist)
+        //users = append(users, "Frequency: 14.074")
+        Log.d("PAYLOAD", message.payload.toString())
+        users = append(users, String(message.payload))
+        var data = message.payload.toString()
+
+        //Log.d("PAYLOAD2", answer.toString())
+
+        mListView.adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, users)
+        mListView.transcriptMode = ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL;
+        mListView.isStackFromBottom = true;
+        mListView.smoothScrollToPosition(mListView.adapter.count -1);
+
+        /*
         onOff = if (String(message.payload) == "on") {
             btnCoffee.setColorFilter(color(R.color.colorPrimary))
             imgCoffee.setImageDrawable(ContextCompat.getDrawable(this@CoffeeActivity, R.drawable.led_on))
@@ -81,7 +110,7 @@ class CoffeeActivity : AppCompatActivity() {
             lytRoot.setBackgroundColor(color(android.R.color.black))
             false
         }
-        progressBar.hide()
+*/
     }
 
     override fun onDestroy() {
